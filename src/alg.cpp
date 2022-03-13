6990abce40251413b93fa4a4f9c31c88d9cae1bc
Vladislav Grachev <99138298+Vladislav-Grachev@users.noreply.github.com>
2022-03-13 20:33:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0, right = size - 1, centrl, g = 0, k = -1;
+  while (left <= right) {
+    centrl = left + (right - left) / 2;
+    if (value == arr[centrl]) {
+      g++;
+      k = centrl;
+      break;
+    }
+    if (value > arr[centrl]) {
+      left = centrl + 1;
+    }
+    else
+      right = centrl - 1;
+  }
+  if (k >=0) {
+    for (int i = 0; i < k; i++) {
+      if (value == arr[i])
+        g++;
+    }
+  }
+  if (g > 0)
+    return g;
+  else
+    retirn 0;
 }
